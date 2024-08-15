2df9d0bdc137348aec4e0d8df58164d2a3be3735
steve-rodri <24379257+steve-rodri@users.noreply.github.com>
2024-08-14 20:04:33 -0400

    🔥 remove __mocks__

--- src/__mocks__/FileContent.ts
+++
@@ -1,5 +0,0 @@
-import { vi } from "vitest"
-
-export class FileContent {
-  generate = vi.fn()
-}
--- src/__mocks__/utils.ts
+++
@@ -1,5 +0,0 @@
-import { vi } from "vitest"
-
-export const writeFile = vi.fn()
-
-export const mockReadFile = vi.fn().mockReturnValue("line1\nline2\nline3")
